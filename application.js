a187b01049a5daaeb8c16e12f5f09fc99c3f25f9
Manuel Avello <manuelavello@Manuels-MacBook-Pro.local>
2017-08-29 15:24:55 +0200

    movimiento y rotacion hechas

--- application.js
+++ application.js
@@ -1,10 +1,20 @@
-var spaceShip;
+//-------------------------------------------------------------
+//---------------- Key functions crontrol ---------------------
+//-------------------------------------------------------------
+
+
+
+var spaceShip1;
+var spaceShip2;
 var keys = {};
+setInterval(checkControls, 100);
 
 $(document).ready(function() {
-  spaceShip= new Spaceship(100, 100, 40);
-  console.log(spaceShip);
-  setInterval(checkControls, 10);
+  spaceShip1 = new Spaceship("ship1", 100);
+  spaceShip2 = new Spaceship("ship2", 100);
+  //   spaceShip= new Spaceship(100, 100, 40, 0);
+  //   console.log(spaceShip);
+
 
 });
 
@@ -14,19 +24,68 @@ $(document).keydown(function(e) {
   delete keys[e.keyCode];
 });
 
-function checkControls() {
-  var a;
-  console.log("entro checkcontrols");
-  if (keys[188]){
-    a = parseInt($("#ship2").css('left')) -4;
-    console.log(a);
-    $("#ship2").css('left', a+'px');
-    spaceShip.moveLeft(parseInt($("#ship2").css('left')));
-  }
-  else if (keys[190]){
-    a=parseInt($("#ship2").css('left')) + 4;
-    $("#ship2").css('left', a+'px');
 
-    spaceShip.moveRight($(".ship"));
+function checkControls() {
+  if (keys[37]) {
+    console.log("it turns right");
+    spaceShip1.rotate("right");
+  } else if (keys[39]) {
+    console.log("it turns left");
+    spaceShip1.rotate("left");
+  } else if (keys[190]) {
+    console.log("it moves right");
+    spaceShip1.move("right");
+  } else if (keys[188]) {
+    console.log("it moves left");
+    spaceShip1.move("left");
+  } else if (keys[50]) {
+    console.log("it turns right");
+    spaceShip2.rotate("right");
+  } else if (keys[49]) {
+    console.log("it turns left");
+    spaceShip2.rotate("left");
+  } else if (keys[68]) {
+    console.log("it moves right");
+    spaceShip2.move("right");
+  } else if (keys[65]) {
+    console.log("it moves left");
+    spaceShip2.move("left");
   }
+
+
+  // first attempts at movement and rotation
+  //   var a;
+  //   if (keys[188]){
+  //     a = parseInt($("#ship2").css('left')) - 4;
+  //     $("#ship2").css('left', a+'px');
+  //     spaceShip.moveLeft();
+  //   }
+  //   else if (keys[190]){
+  //     a=parseInt($("#ship2").css('left')) + 4;
+  //     $("#ship2").css('left', a+'px');
+  //     spaceShip.moveRight();
+  //   }
+  //   else if (keys[65]){
+  //     a = parseInt($("#ship1").css('left')) - 4;
+  //     $("#ship1").css('left', a+'px');
+  //     spaceShip.moveLeft();
+  //   }
+  //   else if(keys[68]){
+  //     a=parseInt($("#ship1").css('left')) + 4;
+  //     $("#ship1").css('left', a+'px');
+  //     spaceShip.moveRight();
+  //   }
+  //   else if (keys[37]){
+  //     spaceShip.rotateRight(e.keycode, this.ship1);
+  // }
+
+
+  // var a;
+  // if (keys[37]){
+  //   a = ($("#ship2").css("transform"));
+  //   console.log(a);
+  //   var f = "rotate(" + a + "deg)";
+  //   $("#ship2").css("transform", "rotate(" + a + "deg)");
+  //   spaceShip.rotateRight();
+  // }
 }
--- spaceship.js
+++ spaceship.js
@@ -1,45 +1,84 @@
-function Spaceship(health,strength,x){
-  this.health = health;
-  this.strength = strength;
-  this.positionX = x;
+console.log("Start the battle!");
+
+
+function Spaceship(name, PlaceSpaceShipYpx){
+  this.name = name;
+  this.posLeft = 0;
+  this.speed = 1;
+  this.posTop = 0;
+  this.speed = 8;
   this.angle = 0;
+  this.posLeft = 0;
+  this.posTop = 0;
+  this.pointAt = 0;
+  this.turnSpeed = 10;
+  this.offX = 10;
+  this.offY = PlaceSpaceShipYpx;
+  this.placeInBoard();
+  // this.strength = strength;
 }
 
-Spaceship.prototype.attack = function(){
-  return this.strength;
+
+Spaceship.prototype.placeInBoard = function(offX, offY){
+  var element = $("<div>").attr("id", this.name).addClass("spaceship");
+  $("#board").append(element);
+  element.css({
+    top: this.posTop + this.offY,
+    left: this.posLeft + this.offX,
+  });
 };
 
-Spaceship.prototype.receiveDamage = function(damage){
-  return this.health -= damage ;
+Spaceship.prototype.rotate = function(direction){
+  if (direction == "right"){
+    $('#' + this.name ).css({
+      transform: "rotate(" + (this.angle += this.turnSpeed) + "deg)"
+    });
+  } else if (direction == "left") {
+    $('#' + this.name ).css({
+      transform: "rotate(" + (this.angle -= this.turnSpeed) + "deg)"
+    });
+  }
 };
 
-Spaceship.prototype.moveRight = function(){
-  console.log('derecha');
-  this.positionX +=8;
+Spaceship.prototype.move = function(direction){
+  if (direction == "right" && this.posLeft <= 510){
+    $('#' + this.name).css({
+      left: this.posLeft += this.offX
+    });
+  } else if(direction == "left" && this.posLeft >= 0){
+    $('#' + this.name).css({
+      left: this.posLeft -= this.offX
+    });
+  }
 };
 
-Spaceship.prototype.moveLeft = function(a){
-  a-=8;
-  this.positionX -=8;
-};
-
-Spaceship.prototype.rotateLeft = function(){
-  this.angle -=1;
-};
-
-Spaceship.prototype.rotateRight = function(){
-  this.angle =1;
-};
-
-
-// var a = parseInt($("#paddle1").css('top'));
-// var paddleHeight = parseInt($(".paddle").css('height'));
-// var boardLowerLimit = parseInt($("#board").css('height'));
-// var lowerLimit = boardLowerLimit - paddleHeight;
-//
-// if (a >= lowerLimit){
-//   return;}
-//   else{
-// a +=8;
-// $("#paddle1").css('top', a +'px');
+Spaceship.prototype.shoot = function (){};
+// first attempt at main methods
 // }
+//
+// Spaceship.prototype.attack = function(){
+//   return this.strength;
+// };
+//
+// Spaceship.prototype.receiveDamage = function(damage){
+//   return this.health -= damage ;
+// };
+//
+// Spaceship.prototype.moveRight = function(){
+//   console.log('derecha');
+//   this.positionX +=4;
+// };
+//
+// Spaceship.prototype.moveLeft = function(a){
+//   a-=4;
+//   this.positionX -=4;
+// };
+//
+// Spaceship.prototype.rotateLeft = function(key, element){
+//   e.keycode === 37 ? this.angle += 5 : this.angle -= 5;
+//   element.css("transform", "rotate(" + this.angle + "deg)");
+// };
+//
+// Spaceship.prototype.rotateRight = function(){
+//   this.angle =1;
+// };
